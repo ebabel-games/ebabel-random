88770f1aa23152073d3b828851590c6e4bd47cdf
Thomas Amar <hello@ebabel.eu>
2018-09-28 23:00:00 +0200

    adjust the documentation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * `random`
  * Returns a random integer number from 1 to 100 or from min to max (min is non-inclusive).
- * @param {number} max - The maximum value that can be randomly returned. This value is included in the possible values.
+ * @param {number?} max - The maximum value that can be randomly returned. This value is included in the possible values.
  * @param {number?} min - The optional minimum value that can be randomly returned. This value is not included in the possible values, therefore to include 0, set min to -1. When min is not set, the minimum value will be 0, so 1 can be returned, which is the most common case in games.
  */
 const random = (max = 100, min = 0) => {
